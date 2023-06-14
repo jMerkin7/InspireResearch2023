f28fbf51b5fdf4035c84a86ecaf8e94e6e3138ff
John Merkin <136014456+jMerkin7@users.noreply.github.com>
2023-06-14 14:15:52 -0400

    Add files via upload

---
+++ StartServer.bat
@@ -0,0 +1,10 @@
+ECHO OFF
+ECHO "Adventures In Minecraft"
+ECHO "Minecraft Server Version is 1.19.3"
+ECHO "  Note - make sure Minecraft is using version 1.19.3"
+ECHO "By continuing you are indicating your agreement to our EULA https://account.mojang.com/documents/minecraft_eula)."
+pause
+cd Server
+start.bat
+pause
+ECHO ON
